3be09c5ee1eb959b54656fd746b8d81bd7d0b925
MatheusJr <bonecodoido339@gmail.com>
2024-03-23 15:22:26 -0300

    Criar Class Produto

--- Ex_list/Program.cs
+++ Ex_list/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace Ex_list
+﻿using System.Data;
+using System.Runtime.CompilerServices;
+
+namespace Ex_list
 {
-    internal class Program
+    class Produto
     {
-        static void Main(string[] args)
+        public Produto(string nome, int quantidade, double valor)
         {
-            Console.WriteLine("Hello, World!");
+            Produto.ValidaQuantidade(quantidade); 
+            Produto.ValidaValor(valor);
+            this._nome = nome;
+            this._quantidade = quantidade;
+
+            private string _nome; 
+            public string Nome { get => _nome; set => _nome = value })
+            
+            private int _quantidade;
+            public int Quantidade
+        {
+            get => _quantidade;
+            set
+            {
+                Produto.ValidaQuantidade(value);
+                _quantidade = value;
+            } 
         }
+
+        private double _valor;
+        public double valor
+        {
+            get=> _valor;
+            set
+            {
+                Produto.ValidaValor(value);
+                _valor = value;
+            }
+        }
+        private static void ValidaQuantidade(int quantidade)
+        {
+            if (quantidade <= 0) 
+            {
+                throw new Exception("Quantidade inválida. Deve ser maior que 0"); 
+
+            }
+        }
+
+        private static void ValidaValor(double valor)
+        {
+            if (valor <= 0)
+            {
+                throw new Exception("Valor inválido. Deve ser maior que 0")
+            }
+        }
+
+
     }
 }
