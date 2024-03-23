9a1cd43e7ef4655d06be062e0edbed670b444613
MatheusJr <bonecodoido339@gmail.com>
2024-03-23 15:34:51 -0300

    Adição Class Parcelamento e Compra

---
+++ Ex_list/Compra.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_list
+{
+    enum FormaPagamento
+    {
+        Pix, 
+        CartaoCredito, 
+        CartaoDebito, 
+        Dinheiro
+    }
+    internal class Compra
+    {
+        List<Produto> produtos;
+        Parcelamento? parcelamento;
+        FormaPagamento formaPagamento; 
+
+        public Compra(List<Produto> produtos, FormaPagamento formaPagamento)
+        {
+            this.produtos = produtos;
+            this.formaPagamento = formaPagamento;
+        }
+    }
+}
---
+++ Ex_list/Parcelamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_list
+{
+    internal class Parcelamento
+    {
+        public int quantidadeDeParcelas;
+        public double valorPorParcela; 
+    }
+}
